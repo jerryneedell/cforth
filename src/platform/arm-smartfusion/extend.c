ec38154cda6450de7d57d479cad8056206a0833e
Jerry Needell <jerryneedell@gmail.com>
2020-04-09 14:09:02 -0400

    add fabric interrupt - fabric-enable, fabric-disable  -- responds to major and minor frame

--- src/platform/arm-smartfusion/extend.c
+++ src/platform/arm-smartfusion/extend.c
@@ -10,8 +10,21 @@
 // FPGA registers - relative to *fgpabase - 0x40050000
 #define FPGADATE 0x80/4
 #define FPGAVERSION 0x84/4
-#define FORTHVERSION 0x0101
+#define FORTHVERSION 0x0002
 #define FORTHDATE 0x7e40409
+#define LED 0
+#define CICCLEAR 0x200C/4
+#define COUNTER 0xA00000
+#define MAJOR_FRAME 0x1
+#define MINOR_FRAME 0x2
+#define  FABRIC_INT_CTRL_BASE_ADDR 0x40051000
+#define  FABRIC_GPIO_BASE_ADDR 0x40050100
+// enable Major Frame and Minor Interrupt
+#define  FABRIC_INTR_ENBL 0x00000003
+#define  FABRIC_INTR_CLR 0xFFFFFFFF
+#define  FABRIC_INTR_ENBL_OFF_ADDR 0x00000020
+#define  FABRIC_INTR_STAT_OFF_ADDR 0x0000002C
+#define  FABRIC_INTR_CLR_OFF_ADDR 0x00000080
 
 volatile unsigned long *fpgabase = (volatile unsigned long *)0x40050000;
 
@@ -135,6 +148,33 @@ cell timer1_init(cell counter)
     MSS_TIM1_load_background(counter);
 }
 
+cell fabric_enable()
+{
+     /* Clear Pending Fabric Interrupts*/
+    NVIC_ClearPendingIRQ(Fabric_IRQn);
+\
+     /* Enable Fabric Interrupt*/
+    NVIC_EnableIRQ(Fabric_IRQn);
+
+
+    /* Configuring the Interrupt control register in FPGA fabric
+     * This will enable the Interrupt in the FPGA*/
+    (*((uint32_t volatile *)(FABRIC_INT_CTRL_BASE_ADDR + FABRIC_INTR_ENBL_OFF_ADDR)) = FABRIC_INTR_ENBL);
+
+}
+
+cell fabric_disable()
+{
+    /* Configuring the Interrupt control register in FPGA fabric
+     * This will disable the Interrupt in the FPGA*/
+    (*((uint32_t volatile *)(FABRIC_INT_CTRL_BASE_ADDR + FABRIC_INTR_CLR_OFF_ADDR)) = FABRIC_INTR_CLR);
+     /* Disabling Fabric Interrupt*/
+    NVIC_DisableIRQ(Fabric_IRQn);
+     /* Clear Pending Fabric Interrupts*/
+    NVIC_ClearPendingIRQ(Fabric_IRQn);
+
+}
+
 cell timer1_start()
 {
     // start timer 1
@@ -181,20 +221,10 @@ cell ((* const ccalls[])()) = {
   C(timer1_stop)     //c timer1-stop     { -- }
   C(timer1_enable)   //c timer1-enable   { -- }
   C(timer1_disable)  //c timer1-disable  { -- }
+  C(fabric_enable)   //c fabric-enable   { -- }
+  C(fabric_disable)  //c fabric-disable  { -- }
 };
 
-
-// Forth words to call the above routines may be created by:
-//
-// system also
-// 0 ccall: spi-send     { i.adr i.len -- }
-
-//  1 ccall: byterev  { s.in -- s.out }
-//
-// and could be used as follows:
-//
-//  5 6 sum .
-//  p" hello"  byterev  count type
 
 
 // Timer1 ISR
@@ -203,6 +233,47 @@ void Timer1_IRQHandler(void)
     // pulse counters
     fpgabase[0x4080/4] = 0x1fffff;
     // toggle LEDs
-    fpgabase[0] ^= 0xff;
+    fpgabase[LED] ^= 0xff;
     MSS_TIM1_clear_irq();
 }
+
+
+
+/* Fabric Interrupt Handler */
+/* This Interrupt handler executes upon the occurrence of
+   Fabric interrupt, which is from the FPGA Fabric.
+   This function reads the status register from the FPGA fabric
+   and checks for the Interrupt source.
+   */
+void Fabric_IRQHandler( void )
+{
+    uint32_t read_irq;
+
+    /* reading Interrupt Status register from Interrupt controller in FPGA fabric */
+    read_irq = (*((uint32_t volatile *)(FABRIC_INT_CTRL_BASE_ADDR + FABRIC_INTR_STAT_OFF_ADDR)));
+    /* Identifying the source of the interrupt */
+    if(read_irq&1)
+    {
+        // toggle LEDs
+        fpgabase[LED] ^= 0xaa;
+        // clear the interrupt
+        fpgabase[CICCLEAR] = MAJOR_FRAME;
+        read_irq &= 0xFFFFFFFE;
+    }
+    if(read_irq&2)
+    {
+        // toggle LEDs
+        fpgabase[LED] ^= 0x55;
+        // clear the interrupt
+        fpgabase[CICCLEAR] = MINOR_FRAME;
+        read_irq &= 0xFFFFFFFD;
+    }
+        read_irq = 0;
+
+    /* Clearing the Interrupts in the FPGA */
+    (*((uint32_t volatile *)(FABRIC_GPIO_BASE_ADDR + FABRIC_INTR_CLR_OFF_ADDR)) = FABRIC_INTR_CLR);
+
+    NVIC_ClearPendingIRQ( Fabric_IRQn );
+
+  }
+
